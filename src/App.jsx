f96dfd95deb8c8aa9ff4c4d331a0c3a13ae91517
jonatas1565 <jonatasferreira330@gmail.com>
2023-03-28 15:15:57 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import './App.css'
+
+
+    function App() {
+      return (
+        <div className="App">
+          <header>
+            <h1>Jônatas Ferreira</h1>
+            <nav>
+              <li className="navbar">Home</li>
+              <li className="navbar">Sobre mim</li>
+              <li className="navbar">Projetos</li>
+              <li className="navbar">Contatos</li>
+            </nav>
+          </header>
+          <main className="main">
+            <div className="sobre">
+              <h2>Olá, Sou o Jônatas I. Ferreira</h2>
+              <p>Sou programador frontend apaixonado por tecnologia</p>
+              <button>Sobre mim</button>
+              <button>Projetos</button>
+               <div>
+                <div className="elipse"></div>
+                <img src="" alt="" />
+                </div>
+            </div>
+            <div className="sobremim">
+              <h2>Sobre mim</h2>
+              <p>Meu nome é Jonatas e sou um programador frontend apaixonado por tecnologia. Com mais de 1 ano de experiência em desenvolvimento web, eu possuo um amplo conhecimento em HTML, CSS e JavaScript, além de frameworks e bibliotecas populares como React.</p>
+              <p>1+ Anos de experiência em tecnologias Frontend</p>
+            </div>
+    
+            <div className="tecnologias">
+              <div className="tecs">
+                <i></i>
+                <h3>html</h3>
+                <p>HTML é a linguagem de marcação usada para criar páginas web.</p>
+
+              </div>
+              <div className="tecs">
+              <i></i>
+                <h3>html</h3>
+                <p>HTML é a linguagem de marcação usada para criar páginas web.</p>
+              </div>
+              <div className="tecs">
+              <i></i>
+                <h3>html</h3>
+                <p>HTML é a linguagem de marcação usada para criar páginas web.</p>
+              </div>
+              <div className="tecs">
+              <i></i>
+                <h3>html</h3>
+                <p>HTML é a linguagem de marcação usada para criar páginas web.</p>
+              </div>
+              <div className="tecs">
+              <i></i>
+                <h3>html</h3>
+                <p>HTML é a linguagem de marcação usada para criar páginas web.</p>
+              </div>
+              <div className="tecs">
+              <i></i>
+                <h3>html</h3>
+                <p>HTML é a linguagem de marcação usada para criar páginas web.</p>
+              </div>
+            </div>
+          </main>
+        </div>
+      );
+    }
+    
+   
+ 
+
+export default App
